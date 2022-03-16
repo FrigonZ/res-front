f53804dc53957ce6eea20001e597a2a4fbfa9299
larrylzhang <larrylzhang@tencent.com>
2022-03-16 21:19:09 +0800

    feat: 新增餐品上下架功能

--- src/components/dish-table/hooks.ts
+++ src/components/dish-table/hooks.ts
@@ -1,7 +1,11 @@
+import { message } from 'antd';
 import { useCallback } from 'react';
 import { useSelector } from 'react-redux';
+import { CGI } from '../../constant/cgi';
+import { DishProps, DishStatus } from '../../constant/entity';
 import { State } from '../../constant/store';
-import { useOpenModal } from '../../page/dish/hooks';
+import { useFetchDishes, useOpenModal } from '../../page/dish/hooks';
+import { usePut } from '../../request/request';
 import { useSetEditDishId, useSetInialDish } from '../../store/dish/hooks';
 import { format2DishForm } from '../../utils/draft';
 
@@ -20,4 +24,42 @@ export const useSetEditMode = () => {
       openModal();
     }
   }, [dishes, setEditId, setInitalDish, openModal]);
+};
+
+export const useToggleDishStatus = () => {
+  const { dishes } = useSelector((state: State) => state.dish);
+  const doPut = usePut();
+  const fetchDishes = useFetchDishes();
+  return useCallback(async (did: string): Promise<boolean> => {
+    const dish = dishes.find((ds) => ds.did === did);
+    if (!dish) {
+      message.error('无餐品数据');
+      return false;
+    }
+
+    const targetDish: DishProps = {
+      ...dish,
+      status: dish.status === DishStatus.NORMAL ? DishStatus.CLOSED : DishStatus.NORMAL,
+    };
+    const resData = await doPut(CGI.DISH, { dish: targetDish });
+
+    if (!resData) {
+      message.error('修改失败');
+      return false;
+    }
+    fetchDishes();
+    return true;
+  }, [dishes, doPut, fetchDishes]);
+};
+
+export const useGetToggleText = () => {
+  const { dishes } = useSelector((state: State) => state.dish);
+  return useCallback((did: string): string => {
+    const dish = dishes.find((ds) => ds.did === did);
+    if (!dish) {
+      return '';
+    }
+
+    return dish.status === DishStatus.NORMAL ? '下架' : '上架';
+  }, [dishes]);
 };
